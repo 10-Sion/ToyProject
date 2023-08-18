f831748f4c740aea54132857e17c834bc847d03d
10-Sion <junhpark.dev@gmail.com>
2023-08-18 09:58:10 +0900

    버전 수정

---
+++ 냠냠/WebContent/app.js
@@ -0,0 +1,71 @@
+const $loginBtn = $('#loginBtn');
+const $registerBtn = $('#registerBtn');
+const $loginPage = $('.login');
+const $form = $('.form');
+
+//  로그인창 클릭 시 나타남
+$loginBtn.on('click', () => {
+  toggleHidden($loginPage);
+  showLoginForm();
+  
+});
+
+// Register 버튼 클릭 시 나타남
+$registerBtn.on('click', () => {
+  toggleHidden($loginPage);
+  showRegisterForm();
+});
+
+
+//  로그인 / 회원가입 창 전환
+$('.message a').click(function(){
+  $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
+
+// 로그인 창 block 상태에서 다른 영역 클릭 시 숨김처리
+$(document).on('click', (e) => {
+  if (
+    !$form.is(e.target) && $form.has(e.target).length === 0 &&
+    !$form.hasClass('hidden') &&
+    !$loginBtn.is(e.target) && !$registerBtn.is(e.target)
+  ) {
+    $loginPage.addClass('hidden');
+  }
+});
+
+/* 로그인 창 스크롤 반응 */
+$(window).scroll(() => {
+  const position = $(window).scrollTop();
+  if (!$loginPage.hasClass('hidden')) {
+    $form.stop().animate({
+      'top': position+200 + 'px'
+    }, 550);
+  }
+});
+
+
+
+//  메인 페이지 메뉴창
+$(document).ready(function(){
+    $(".menu-button").click(function(){
+    $(".menu-bar").toggleClass( "open" );
+  })
+})
+
+
+
+//  보조 함수
+function toggleHidden(page) {
+  page.removeClass('hidden');
+
+}
+
+function showLoginForm() {
+  $('.login-form').show(500);
+  $('.register-form').hide(500);
+}
+
+function showRegisterForm() {
+  $('.register-form').show(500);
+  $('.login-form').hide(500);
+}

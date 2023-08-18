28638e46c953429aeb82f1f85ac271c125ec82c9
10-Sion <66767165+10-Sion@users.noreply.github.com>
2023-08-18 16:51:46 +0900

    마이페이지 업뎃

--- 냠냠/WebContent/app.js
+++ 냠냠/WebContent/app.js
@@ -7,7 +7,6 @@ const $form = $('.form');
 $loginBtn.on('click', () => {
   toggleHidden($loginPage);
   showLoginForm();
-  
 });
 
 // Register 버튼 클릭 시 나타남
@@ -16,18 +15,19 @@ $registerBtn.on('click', () => {
   showRegisterForm();
 });
 
-
 //  로그인 / 회원가입 창 전환
-$('.message a').click(function(){
-  $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+$('.message a').click(function () {
+  $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
 });
 
 // 로그인 창 block 상태에서 다른 영역 클릭 시 숨김처리
 $(document).on('click', (e) => {
   if (
-    !$form.is(e.target) && $form.has(e.target).length === 0 &&
+    !$form.is(e.target) &&
+    $form.has(e.target).length === 0 &&
     !$form.hasClass('hidden') &&
-    !$loginBtn.is(e.target) && !$registerBtn.is(e.target)
+    !$loginBtn.is(e.target) &&
+    !$registerBtn.is(e.target)
   ) {
     $loginPage.addClass('hidden');
   }
@@ -37,27 +37,25 @@ $(document).on('click', (e) => {
 $(window).scroll(() => {
   const position = $(window).scrollTop();
   if (!$loginPage.hasClass('hidden')) {
-    $form.stop().animate({
-      'top': position+200 + 'px'
-    }, 550);
+    $form.stop().animate(
+      {
+        top: position + 200 + 'px',
+      },
+      550
+    );
   }
 });
 
-
-
 //  메인 페이지 메뉴창
-$(document).ready(function(){
-    $(".menu-button").click(function(){
-    $(".menu-bar").toggleClass( "open" );
-  })
-})
-
-
+$(document).ready(function () {
+  $('.menu-button').click(function () {
+    $('.menu-bar').toggleClass('open');
+  });
+});
 
 //  보조 함수
 function toggleHidden(page) {
   page.removeClass('hidden');
-
 }
 
 function showLoginForm() {
@@ -69,3 +67,15 @@ function showRegisterForm() {
   $('.register-form').show(500);
   $('.login-form').hide(500);
 }
+
+//  마이페이지 리스트 분류 처리
+$(document).ready(function () {
+  $('.data-item').click(function () {
+    const index = $(this).index('.data-item');
+
+    $('.item-details').css('display', 'block');
+
+    $('.item-details .item').hide();
+    $('.item-details .item').eq(index).show();
+  });
+});
